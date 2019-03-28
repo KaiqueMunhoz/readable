379c3ff5e478b0a9b2bf68d3564b3a807ae62bb9
Kaique Munhoz <munhoz13km@gmail.com>
2019-03-28 03:30:12 -0300

    Feat: Utilizando o Provider e passando 'store' como argumento

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//Routes
 import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes';
+import Routes from './config/routes';
+
+//Redux
+import store from './config/store'
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
+  <Provider store={store()}>
     <BrowserRouter>
         <Routes />
     </BrowserRouter>
-    , document.getElementById('root'));
+  </Provider>
+  , document.getElementById('root'));
