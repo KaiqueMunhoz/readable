368ab72858817c6ea3e78dc514c56dd5e289113c
Kaique Munhoz <munhoz13km@gmail.com>
2019-03-28 01:04:56 -0300

    Fix: Alterado items visuais da postagem

--- frontend/src/components/PostHeader/index.js
+++ frontend/src/components/PostHeader/index.js
@@ -8,17 +8,13 @@ const PostHeader = () => {
       <h2 class="post-title">Introducing Pure</h2>
 
       <p class="post-meta">
-        By{" "}
-        <a href="#">
-          Tilo Mitra
-        </a>{" "}
-        under{" "}
-        <a class="post-category post-category-design" href="#">
-          CSS
-        </a>{" "}
-        <a class="post-category post-category-pure" href="#">
-          Pure
-        </a>
+        By <span>Tilo Mitra</span> under
+        <button class="post-category post-category-design" >
+          Up
+        </button>
+        <button class="post-category post-category-js">
+          Down
+        </button>
       </p>
     </header>
   )
