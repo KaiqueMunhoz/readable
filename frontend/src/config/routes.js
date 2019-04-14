f36073f65566f419a9bf8394cd112474f2572d9a
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-14 16:26:33 -0300

    Fix: Ajustando rotas, nao é necessario passar 'props'

--- frontend/src/config/routes.js
+++ frontend/src/config/routes.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 //Pages
-import Home from '../pages/Home'
-import PostNew from '../pages/PostNew'
-import PostDetails from '../pages/PostDetails'
+import Home from "../pages/Home";
+import PostNew from "../pages/PostNew";
+import PostDetails from "../pages/PostDetails";
 
-const Routes = (props) => {
-
+const Routes = () => {
   return (
     <Switch>
-      <Route path='/' exact render={() => ( <Home {...props}/> )} />
-      <Route path='/new' render={() => ( <PostNew {...props}/> )} />
-      <Route path='/:category' render={() => ( <Home {...props}/> )} />
-      <Route path='/:category/:post_id' render={() => ( <PostDetails {...props}/> )} />
-      <Route path='/edit/:post_id' render={() => ( <PostNew {...props}/> )} />
+      <Route path="/" exact render={() => <Home />} />
+      <Route path="/new" render={() => <PostNew />} />
+      <Route path="/:category" render={() => <Home />} />
+      <Route path="/:category/:post_id" render={() => <PostDetails />} />
+      <Route path="/edit/:post_id" render={() => <PostNew />} />
     </Switch>
   );
-}
+};
 
 export default Routes;
