175b894d86d014a4364feabebc045a0b63e91ed5
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-14 15:54:04 -0300

    Fix: Voltando page Home para stateless componet

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -5,36 +5,17 @@ import Posts from "../../components/Posts";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import * as PostsActions from "../../store/actions/posts";
+const Home = () => {
+  return (
+    <div class="pure-g">
+      <Header />
+      <main class="content pure-u-1 pure-u-md-3-4">
+        <Navbar />
+        <Posts />
+        <Footer />
+      </main>
+    </div>
+  );
+};
 
-class Home extends React.Component {
-  componentDidMount() {
-    this.props.postsRequest();
-  }
-  render() {
-    return (
-      <div class="pure-g">
-        <Header />
-        <main class="content pure-u-1 pure-u-md-3-4">
-          <Navbar />
-          <Posts />
-          <Footer />
-        </main>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  posts: state.posts
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(PostsActions, dispatch);
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
+export default Home;
