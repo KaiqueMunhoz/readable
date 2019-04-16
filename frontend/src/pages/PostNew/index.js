4db9c2074bc9eb45ecace11bdb04eb1602838f01
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-16 08:07:06 -0300

    Feat: Importando componentes na tela de nova postagem

--- frontend/src/pages/PostNew/index.js
+++ frontend/src/pages/PostNew/index.js
@@ -1,51 +1,16 @@
-import React from "react";
-import Modal from "../../components/Modal";
+import React, { Fragment } from "react";
+import Form from "../../components/Form";
+import Header from "../../components/Header";
+import Navbar from "../../components/Navbar";
 
-const PostNew = () => {
-  return (
-    <Modal isOpen={true}>
-      <div>
-        <form className="pure-form">
-          <fieldset className="pure-group">
-            <input
-              type="text"
-              className="pure-input-1-2"
-              placeholder="Username"
-            />
-            <input
-              type="text"
-              className="pure-input-1-2"
-              placeholder="Password"
-            />
-            <input
-              type="email"
-              className="pure-input-1-2"
-              placeholder="Email"
-            />
-          </fieldset>
-
-          <fieldset className="pure-group">
-            <input
-              type="text"
-              className="pure-input-1-2"
-              placeholder="A title"
-            />
-            <textarea
-              className="pure-input-1-2"
-              placeholder="Textareas work too"
-            />
-          </fieldset>
-
-          <button
-            type="submit"
-            className="pure-button pure-input-1-2 pure-button-primary"
-          >
-            Sign in
-          </button>
-        </form>
-      </div>
-    </Modal>
-  );
-};
+const PostNew = () => (
+  <div className="pure-g">
+    <Header />
+    <main className="content pure-u-1 pure-u-md-3-4">
+      <Navbar title="New Post" />
+      <Form />
+    </main>
+  </div>
+);
 
 export default PostNew;
