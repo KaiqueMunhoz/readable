4f09f0f3bde51ae22e9ed4331bd7f922b489e05d
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-16 07:31:24 -0300

    Feat: Utilizando DateHelper no cabeçalho da postagem

--- frontend/src/components/PostHeader/index.js
+++ frontend/src/components/PostHeader/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import DateHelper from "../../helpers/dateHelper";
 
 const PostHeader = ({ title, timestamp, author }) => {
   return (
@@ -7,7 +8,7 @@ const PostHeader = ({ title, timestamp, author }) => {
       <h2 className="post-title">{title}</h2>
 
       <p className="post-meta">
-        By <span>{author}</span> published {timestamp}
+        By <span>{author}</span> published {DateHelper(timestamp)}
         <button className="post-category post-category-design">Up</button>
         <button className="post-category post-category-js">Down</button>
       </p>
