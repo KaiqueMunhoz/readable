4a45cefb4484083185a21ed4b6752ed1a4fd94e7
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-16 08:40:56 -0300

    Fix: Ajuste no reducer que criava sempre mais categorias

--- frontend/src/store/reducers/categories.js
+++ frontend/src/store/reducers/categories.js
@@ -1,14 +1,11 @@
-import {
-  CATEGORIES_SUCCESS
-} from "../actions/categories";
+import { CATEGORIES_SUCCESS } from "../actions/categories";
 import initialState from "../initialState";
 
 export default function categories(state = initialState.categories, action) {
   switch (action.type) {
-    case CATEGORIES_SUCCESS:
-      {
-        return [...state, ...action.payload.data];
-      }
+    case CATEGORIES_SUCCESS: {
+      return [...action.payload.data];
+    }
     default:
       return state;
   }
