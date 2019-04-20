19c2305b0b6532e879e089126ed54052cefade09
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-20 15:07:46 -0300

    Feat: Esboço da tela PostDetails

--- frontend/src/pages/PostDetails/index.js
+++ frontend/src/pages/PostDetails/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import Post from "../../components/Posts/PostsByCategory/Post";
+import Header from "../../components/Header";
+import Navbar from "../../components/Navbar";
 
 const PostDetails = () => {
-
-  return(
-    <div></div>
-  )
-}
+  return (
+    <div className="pure-g">
+      <Header />
+      <main className="content pure-u-1 pure-u-md-3-4">
+        <Navbar />
+        <Post />
+      </main>
+    </div>
+  );
+};
 
 export default PostDetails;
