8bd97164d8adb19c826e038cd112c07602821cab
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-24 20:29:28 -0300

    Feat: Importando saga

--- frontend/src/store/sagas/index.js
+++ frontend/src/store/sagas/index.js
@@ -1,22 +1,24 @@
-import { all, takeLatest, take } from "redux-saga/effects";
+import { all, takeLatest } from "redux-saga/effects";
 
 //SAGAS
 import { categories } from "./categories";
-import { posts, postsByCategory, postsDetails } from "./posts";
+import { posts, postsByCategory, postsDetails, postsUpdateVote } from "./posts";
 
 //ACTION TYPES
 import { CATEGORIES_REQUEST } from "../actions/categories";
 import {
   POSTS_REQUEST,
   POSTS_CATEGORY_REQUEST,
-  POSTS_DETAILS_REQUEST
+  POSTS_DETAILS_REQUEST,
+  POST_UPDATE_VOTE_REQUEST
 } from "../actions/posts";
 
 export default function* rootSaga() {
   yield all([
     takeLatest(POSTS_REQUEST, posts),
     takeLatest(POSTS_CATEGORY_REQUEST, postsByCategory),
-    take(POSTS_DETAILS_REQUEST, postsDetails),
+    takeLatest(POST_UPDATE_VOTE_REQUEST, postsUpdateVote),
+    takeLatest(POSTS_DETAILS_REQUEST, postsDetails),
     takeLatest(CATEGORIES_REQUEST, categories)
   ]);
 }
