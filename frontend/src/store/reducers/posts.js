e6b450a0f442c3bd768e8184764b1eb5484d7cb3
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-24 20:29:39 -0300

    Feat: Criando reducer

--- frontend/src/store/reducers/posts.js
+++ frontend/src/store/reducers/posts.js
@@ -1,7 +1,8 @@
 import {
   POSTS_SUCCESS,
   POSTS_CATEGORY_SUCCESS,
-  POSTS_DETAILS_SUCCESS
+  POSTS_DETAILS_SUCCESS,
+  POST_UPDATE_VOTE_SUCCESS
 } from "../actions/posts";
 
 const INITIAL_STATE = [];
@@ -14,6 +15,8 @@ export default function posts(state = INITIAL_STATE, action) {
       return [...action.payload.data];
     case POSTS_DETAILS_SUCCESS:
       return [...action.payload.data];
+    case POST_UPDATE_VOTE_SUCCESS:
+      return [action.payload.data];
     default:
       return state;
   }
