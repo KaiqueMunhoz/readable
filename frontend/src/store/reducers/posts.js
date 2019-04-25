bb81ac7f74976a738134d3c8a272d00f944bcb57
Kaique Munhoz <munhoz13km@gmail.com>
2019-04-25 00:20:16 -0300

    Fix: Ajustado reducer que mostra postagens

--- frontend/src/store/reducers/posts.js
+++ frontend/src/store/reducers/posts.js
@@ -11,7 +11,7 @@ const INITIAL_STATE = [];
 export default function posts(state = INITIAL_STATE, action) {
   switch (action.type) {
     case POSTS_SUCCESS:
-      return [...state, ...action.payload.data];
+      return [...action.payload.data];
     case POSTS_CATEGORY_SUCCESS:
       return [...action.payload.data];
     case POSTS_DETAILS_SUCCESS:
